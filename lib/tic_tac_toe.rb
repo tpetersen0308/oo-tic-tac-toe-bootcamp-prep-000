9777ca28ae848ac1a6348de2a99aca03bca465b6
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-11 00:57:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -91,7 +91,7 @@ class TicTacToe
       turn
     end
     if won?
-      puts "Congratulations, #{winner}!"
+      puts "Congratulations #{winner}!"
     elsif draw?
       puts "Cat's Game!"
     end
